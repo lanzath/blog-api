7a46fd9d5108b14e4541f63b46ca3a4939dcc4cd
Thiago Lanza <lanzathm@gmail.com>
2022-01-11 19:45:12 -0300

    Basic CRUD (1 model) example with validations

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,14 +1,145 @@
 ﻿using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers;
 
-[Route("api/categories")]
+[Route("api/v1/categories")]
 [ApiController]
 public class CategoryController : ControllerBase
 {
     [HttpGet]
-    public IActionResult Get([FromServices] BlogDataContext context)
-        => Ok(context.Categories.AsNoTracking().ToList());
+    public async Task<IActionResult> Get([FromServices] BlogDataContext context)
+    {
+        try
+        {
+            var categories = await context.Categories.AsNoTracking().ToListAsync();
+            return Ok(new ResultViewModel<List<Category>>(categories));
+
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Category>>("Falha interna no servidor"));
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById([FromServices] BlogDataContext context, int id)
+    {
+        try
+        {
+            var category = await context
+                .Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound(new ResultViewModel<Category>("Conteúdo não encontrado"));
+
+            return Ok(new ResultViewModel<Category>(category));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Category>>("Falha interna no servidor"));
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Insert(
+        [FromServices] BlogDataContext context,
+        [FromBody] EditorCategoryViewModel model
+        )
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+
+        try
+        {
+            var category = new Category
+            {
+                Id = 0,
+                Name = model.Name,
+                Slug = model.Slug.ToLower()
+            };
+            await context.Categories.AddAsync(category);
+            await context.SaveChangesAsync();
+
+            return Created($"{category.Id}", new ResultViewModel<Category>(category));
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Não foi possível incluir a categoria"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Category>>("Falha interna no servidor"));
+        }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Update(
+        int id,
+        [FromServices] BlogDataContext context,
+        [FromBody] EditorCategoryViewModel model
+        )
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+
+        try
+        {
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id != id);
+
+            if (category == null)
+                return NotFound(new ResultViewModel<Category>("Conteúdo não encontrado"));
+
+            category.Name = model.Name;
+            category.Slug = model.Slug;
+
+            context.Update(category);
+            await context.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<Category>(category));
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Não foi possível atualizar a categoria"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Category>>("Falha interna no servidor"));
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id, [FromServices] BlogDataContext context)
+    {
+        try
+        {
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id != id);
+
+            if (category == null)
+                return NotFound();
+
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<Category>(category));
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Não foi possível remover a categoria"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Category>>("Falha interna no servidor"));
+        }
+    }
 }
---
+++ Extensions/ModelStateExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Blog.Extensions;
+
+public static class ModelStateExtension
+{
+    // Com a classe e métodos estáticos, ao utilizar o this no parâmetro é possível adicionar esse método a uma outra classe
+    // a qual o tipo foi declarado após o this.
+    public static List<string> GetErrors(this ModelStateDictionary modelState)
+    {
+        var result = new List<string>();
+        foreach (var item in modelState.Values)
+            result.AddRange(item.Errors.Select(error => error.ErrorMessage)); // Método do Linq para adicionar a um array um range de valores.
+        return result;
+    }
+}
